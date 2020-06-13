92cf395a7965dd4b9fad6ad149d0a80448ee086c
lixiaoming <1404241558@qq.com>
2020-06-13 16:45:06 +0800

    refactor: 修改推送、自动化构建脚本

--- deploy.sh
+++ deploy.sh
@@ -28,15 +28,15 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
 # deploy to coding
-echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:xgy/xgy.git
+echo 'bilent.top' > CNAME  # 自定义域名
+if [ -z "$MAYUN_TOKEN" ]; then  # -z 字符串 长度为0则为true；$MAYUN_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  mayunUrl=git@gitee.com:lixiaoming888888/web-blog.git
 else
-  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+  mayunUrl=https://gitee.com/lixiaoming888888/web-blog.git
 fi
 git add -A
 git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+git push -f $mayunUrl master # 推送到码云
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
